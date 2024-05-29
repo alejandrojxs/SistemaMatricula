ba03714b9a9f942a9331a69f1752cded5241c61f
Dell <Dell@DESKTOP-B3LB8F1>
2024-05-28 21:47:15 -0600

    Agregar archivos de proyecto.

---
+++ SistemaMatricula/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+        Dim con As New OleDbConnection("Provider=Microsoft.jet.Oledb.4.0; Data source=C:\Users\Dell\Downloads\SistemaMatricula-master\Base de datos\Matricula.mdb")
+        Try
+            con.Open()
+            MsgBox("Conectado")
+
+            Dim query = "Select * from Alumnos"
+            Dim adap As New OleDbDataAdapter(query, con)
+            Dim dt As New DataTable
+            adap.Fill(dt)
+            DataGridView1.DataSource = dt
+
+
+        Catch ex As Exception
+
+            MsgBox("No se econectó por:" & ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Form2.Show()
+    End Sub
+End Class
---
+++ SistemaMatricula/Grafica.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.OleDb
+Public Class Grafica
+    Private Sub Grafica_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim conexion As New OleDbConnection("Provider=Microsoft.Jet.Oledb.4.0; Data Source=C:\Users\Dell\Downloads\SistemaMatricula-master\Base de datos\Matricula.mdb")
+        Try
+            conexion.Open()
+            MsgBox("Conectado")
+
+            Dim query = "Select * from Alumnos"
+            Dim adap As New OleDbDataAdapter(query, conexion)
+            Dim dt As New DataTable
+            adap.Fill(dt)
+            Chart1.Series("Series1").XValueMember = "Nombre"
+            Chart1.Series("Series1").YValueMembers = "edadEstudiante"
+            Chart1.DataSource = dt
+
+
+        Catch ex As Exception
+            MsgBox("No se conecto por: " & ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub Chart1_Click(sender As Object, e As EventArgs) Handles Chart1.Click
+
+    End Sub
+End Class
